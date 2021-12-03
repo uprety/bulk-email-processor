ebe97bdefdf6ccb650738a567e7c5c58f260e93e
uprety <dupreti27@gmail.com>
2021-12-03 16:49:32 +0545

    completed frontend

--- frontend/src/components/ProcessFile/EmailLogs.js
+++ frontend/src/components/ProcessFile/EmailLogs.js
@@ -1,10 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Table from 'react-bootstrap/Table'
+import axios from 'axios'
+import io from 'socket.io-client'
 
-const EmailLogs = () => {
-    return (
-        <>
-        </>
-    )
+const socket = io(process.env.REACT_APP_SERVER_URL)
+
+const EmailLogs = (props) => {
+  const [tableLogs, setTableLogs] = useState([]);
+
+  useEffect(() => {
+    axios.get(process.env.REACT_APP_SERVER_URL + '/api/get-sent-email-logs', { withCredentials: true })
+      .then((response) => {
+        // Sent logs is a list 
+        setTableLogs(response.data.sentLogs)
+      })
+  }, [])
+
+
+  useEffect(() => {
+    console.log(props.initiator)
+    socket.on(props.initiator, log => {
+      if (log) {
+        setTableLogs([...tableLogs, log])
+      }
+      console.log(`${props.initiator} ${log}`)
+    });
+
+  return () => {
+    socket.off(props.initiator);
+  };
+}, );
+
+return (
+  <>
+    <h3>
+      Mail Sent Logs
+    </h3>
+
+    <div className="table-responsive">
+      <Table striped bordered hover className="table mt-2">
+        <tbody>
+          {tableLogs.map((tableLog, index) => (
+            <tr key={index} style={{ textAlign: 'left' }}>
+              <td>{`${index}. ${tableLog}`}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  </>
+)
 }
 
 export default EmailLogs
